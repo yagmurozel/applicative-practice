78bb8f2ac891c85fcc9d9c43de572c95d7b26da5
jjhiggz <jonathan.higger@gmail.com>
2023-01-11 16:29:49 -0700

    reworded e11 slightly

--- exercises/e11.js
+++ exercises/e11.js
@@ -2,13 +2,12 @@ import { data } from "../data/data";
 
 // SPACE DATA EXERCISE 11
 // Return an array of Planets' names with less than 10 moons
+// include planets with 0 moons
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function lowMoonsPlanets(data) {
   // Your code goes here...
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-11"
